04fa43491c2037cb384c64abd0529941bb67b905
lucidbard <john@lucidbard.com>
2023-02-16 00:02:54 +0000

    Feb 15, 2023, 7:02 PM

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const configuration = new Configuration({
     apiKey: process.env.OPENAI_API_KEY,
 });
 const openai = new OpenAIApi(configuration);
+
 const response = await openai.createCompletion({
   model: "text-davinci-003",
   prompt: process.argv[2],
